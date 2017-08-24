e043f35e7200a30f0cb5195fcbaf88d2429ba3e8
RENT-Komputer\RENT <jan.kisiel22@gmail.com>
2017-08-24 20:47:48 +0200

    no message

--- src/ENUM/ZAD4/DayofTheWeek.java
+++ src/ENUM/ZAD4/DayofTheWeek.java
@@ -8,24 +8,26 @@ package ENUM.ZAD4;
 public class DayofTheWeek {
     public static void main(String[] args) {
         for(DayofTheWeek.DayofWeek dzienTygodnia : DayofTheWeek.DayofWeek.values()){
-            System.out.printf("%s - %s - %s \n", dzienTygodnia.polskienazwy, dzienTygodnia, dzienTygodnia.niemieckie);
+            System.out.printf("%s - %s - %s - %s\n", dzienTygodnia.polskienazwy, dzienTygodnia, dzienTygodnia.niemieckie, dzienTygodnia.next());
         }
 
     }
 
     enum DayofWeek{
-        Mondey("Poniedzialek", "Montag"),
-        Twusdey("Wtorek", "Dienstang"),
-        Wendesday("Sroda", "Mittwoch"),
-        Thrusday("Czwartek", "Donnerstag"),
-        Friday("Piatek", "Freitag"),
-        Saturday("Sobota", "Samstag"),
-        Sunday("Niedziela", "Sonntag"),;
+        Mondey("Poniedzialek", "Montag") {DayofWeek next(){return Thrusday;}},
+        Twusdey("Wtorek", "Dienstang"){ DayofWeek next(){return Wendesday;}},
+        Wendesday("Sroda", "Mittwoch"){ DayofWeek next(){return Thrusday;}},
+        Thrusday("Czwartek", "Donnerstag"){  DayofWeek next(){return Friday;}},
+        Friday("Piatek", "Freitag"){  DayofWeek next(){return Saturday;}},
+        Saturday("Sobota", "Samstag"){  DayofWeek next(){return Sunday;}},
+        Sunday("Niedziela", "Sonntag"){  DayofWeek next(){return Mondey;}};
 
+        abstract DayofWeek next();
         private final String polskienazwy, niemieckie;
         DayofWeek(String polskienazwy, String niemieckie) {
             this.polskienazwy = polskienazwy;
             this.niemieckie = niemieckie;
+
         }
     }
 }
