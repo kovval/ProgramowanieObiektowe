8dc627afbf9a3388a202e040ff2f894452c96e24
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-09-13 11:55:33 +0200

    no message

--- src/Strategia/Sorter/quickSort.java
+++ src/Strategia/Sorter/quickSort.java
@@ -4,9 +4,7 @@ public class quickSort implements ISort {
 
     @Override
     public int[] sort(int[] tab) {
-
         quicksort(tab, 0, tab.length-1);
-
         return tab;
     }
 
@@ -15,7 +13,6 @@ public class quickSort implements ISort {
         int j = right;
         int temp;
         int v = tablica[(left + right) / 2];
-
         do {
             while (tablica[i] < v) i++;
             while (tablica[j] > v) j--;
@@ -31,39 +28,5 @@ public class quickSort implements ISort {
         if (left < j) quicksort(tablica, left, j);
         if (i < right) quicksort(tablica, i, right);
     }
-
 }
-
-
-
-
-
-//    public static void main(String[] args) {
-//        int i, liczba;
-//        Scanner sc = new Scanner(System.in);
-//        Random rd = new Random();
-//
-//        System.out.println("Ile liczb chesz posortowac: ");
-//
-////        count = sc.nextInt();
-//        count = 20;
-//        _tablica = new int[count];
-//        for(i=0; i<count; i++) {
-//            _tablica[i] = rd.nextInt(50);
-//        }
-//
-//        System.out.print("Tablica przed posortowaniem: ");
-//        for(i=0; i<count; i++) {
-//            System.out.print(_tablica[i] + " ");
-//            quicsort(_tablica, 0, count - 1);
-//        }
-//        System.out.println();
-//        System.out.print("Tablica po posortowaniu: " + " ");
-//        for(i=0; i<count; i++)
-//            System.out.print(_tablica[i]+" ");
-//
-//    }
-//
-//}
-
 
