6b51bb00a2af53cd4d74679af898e9ccb7d05076
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-09-17 18:56:46 +0200

    no message

--- src/PiatekChalange/Flamaster.java
+++ src/PiatekChalange/Flamaster.java
@@ -2,7 +2,7 @@ package PiatekChalange;
 
 public class Flamaster {
 
-    private static String word = ("AAAAAAAAAAAASSSSSSSSSSSSSSSSddFFFFFersdsdkkkkkkk");
+    private static String word = ("Abbbhhhkkk");
 
     public static void main(String[] args) {
 
@@ -11,26 +11,28 @@ public class Flamaster {
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i <chars.length; i++){
+
             int countWord = 0;
             if(i+1 >= chars.length  ){
                 sb.append(chars[i]);
                break;
             }
             countWord++;
-            if (chars[i] == chars[i+1]) {
-                sb.append(chars[i]);
-                countWord++;
-                while(chars[i]==chars[i+1]  && (i+2 < chars.length)) {
+            sb.append(chars[i]);
+                while(chars[i] == chars[i+1]) {
                     countWord++;
                     i++;
+                    if(i+1 >= chars.length){
+                        break;
+                    }
                 }
                 if(countWord == 2){
-                    i++;
-                } else if(countWord > 2) {
+                    sb.append(chars[i]);
+                } else if(countWord > 2 ) {
                     sb.append(countWord);
-                    i++;
-                }
+
             }
+
         }
 
         System.out.println(sb);
