f3860b5fd310025100d9c43c551284a081e54b7b
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-09-29 03:28:08 +0200

    no message

---
+++ src/Simple/GameTest.java
@@ -0,0 +1,24 @@
+package Simple;
+
+/***********************
+ *  Assignment 4
+ *  Andreas Hadjigeorgiou
+ *  ahh2131
+ *  Poker
+ */
+
+public class GameTest 
+{
+
+  public static void main(String[] args) 
+	{
+		
+		// make game
+		Game game = new Game();
+				
+		// play game
+		game.play();
+
+	}
+
+}
