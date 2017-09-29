61f8268b95b397597c6c110f4780285126488d62
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-09-29 03:54:55 +0200

    no message

--- src/Account/ChallangeZProgramingCom.java
+++ src/Account/ChallangeZProgramingCom.java
@@ -3,10 +3,15 @@ package Account;
 import java.util.Scanner;
 
 public class ChallangeZProgramingCom {
-    public static void main(String[] args) {
 
+
+    private String scanText(){
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
+        return word;
+    }
+
+    private void separateMetody(String word){
         System.out.print("[\"" + word.charAt(0));
         for (int i = 0; i < word.length() - 1; i++) {
 
@@ -15,5 +20,28 @@ public class ChallangeZProgramingCom {
             } else System.out.print("\", \"" + word.charAt(i + 1));
         }
         System.out.print("\"]");
+    }
+
+    private void metodySeparator(String word){
+
+        for (int i = word.length()-1; i > 0 ; i--) {
+            if (word.charAt(i) == word.charAt(i-1)) {
+                System.out.print(word.charAt(i-1));
+            } else {
+                System.out.print(word.charAt(i) + " ");
+            }
+        }
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        ChallangeZProgramingCom first = new ChallangeZProgramingCom();
+
+        first.separateMetody( "aaasssssdddddders");
+//        first.metodySeparator("dddddssssserds");
+
     }
 }
